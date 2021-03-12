e30626ac4171e5c6f2a0dd8454e95690871d78b6
Brent Lee <brentlee@Brents-MacBook-Pro.local>
2021-03-11 21:53:42 -0800

    adding a home button

--- components/footer.js
+++ components/footer.js
@@ -29,6 +29,7 @@ footerTemplate.innerHTML = `
 <footer>
     <nav>
         <ul>
+            <li><a href="index.html">Home</a></li>
             <li><a href="about.html">About</a></li>
             <li><a href="work.html">Work</a></li>
             <li><a href="contact.html">Contact</a></li>
